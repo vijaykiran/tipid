82a4d1f78767fdcbae365ea9e0ac41adf3de05aa
vijaykiran <mail@vijaykiran.com>
2012-10-11 20:43:36 +0200

    Init

---
+++ src/tipid/core.clj
@@ -0,0 +1,6 @@
+(ns tipid.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/tipid/core_test.clj
@@ -0,0 +1,7 @@
+(ns tipid.core-test
+  (:use clojure.test
+        tipid.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
