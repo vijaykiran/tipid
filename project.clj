7f87ae6fcb41a9435776cd3fa4a90d93afd0ae32
vijaykiran <mail@vijaykiran.com>
2012-10-11 20:45:30 +0200

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject tipid "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "tipid - the tipi server"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url "http://tipi.vijaykiran.com"}
   :dependencies [[org.clojure/clojure "1.4.0"]])
